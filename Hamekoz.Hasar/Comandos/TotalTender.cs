ab67818590e70f138ac13390fa7d948658118a76
Ezequiel Taranto <ezequiel89@gmail.com>
2014-07-22 11:17:37 -0300

    Add library for Hasar Fiscal Printers

---
+++ Hamekoz.Hasar/Comandos/TotalTender.cs
@@ -0,0 +1,45 @@
+﻿//
+//  TotalTender.cs
+//
+//  Author:
+//       Ezequiel Taranto <ezequiel89@gmail.com>
+//
+//  Copyright (c) 2014 etaranto
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+namespace Hamekoz.Hasar
+{
+	public class TotalTender : Comando
+	{
+		const string cmd = "D";
+		public float Vuelto {get;set;}
+		string texto;
+		float montoPagado;
+		string cancelacionOVuelto;
+
+		public string Comando()
+		{
+			return string.Format("{0}{1}{2}{1}{3:###0.00}{1}{4}{1}{5}",cmd,separador,texto,montoPagado,cancelacionOVuelto,0).Replace(",",".");
+		}
+		public TotalTender (string texto, float montoPagado,string cancelacionOVuelto)
+		{
+			this.texto = texto;
+			this.montoPagado = montoPagado;
+			this.cancelacionOVuelto = cancelacionOVuelto;
+		}
+	}
+}
+
---
+++ Hamekoz.Hasar/Enumerados/Imprime.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace POS.Fiscal
+{
+	
+	
+	public enum OpcionComando
+	{
+		SI,// = "P",
+		NO// = "A"
+	}
+}
