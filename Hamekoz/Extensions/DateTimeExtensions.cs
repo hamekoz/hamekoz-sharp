d4dceed193cb7dcfc19444ea111591a592899948
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2014-11-13 22:18:14 -0300

    Add Hamekoz project for commons basics classes. Add DateTimeExtensions Add StringExtensions

---
+++ Hamekoz/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,71 @@
+﻿//
+//  DateTimeExtensions.cs
+//
+//  Author:
+//       Claudio Rodrigo Pereyra Diaz <rodrigo@hamekoz.com.ar>
+//
+//  Copyright (c) 2014 Hamekoz
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Globalization;
+
+namespace Hamekoz.Extensions
+{
+	public static class DateTimeExtensions
+	{
+		/// <summary>
+		/// Weeks the of year with Sunday as first day of week.
+		/// </summary>
+		/// <returns>The number of week.</returns>
+		/// <param name="date">Date.</param>
+		public static int WeekOfYear(this DateTime date)
+		{
+			Calendar calendario = DateTimeFormatInfo.CurrentInfo.Calendar;
+			return calendario.GetWeekOfYear (date, CalendarWeekRule.FirstFullWeek, DayOfWeek.Sunday);
+		}
+
+		/// <summary>
+		/// Weeks the of year.
+		/// </summary>
+		/// <returns>The number of week.</returns>
+		/// <param name="date">Date.</param>
+		/// <param name="firstDay">First day of week.</param>
+		public static int WeekOfYear(this DateTime date, DayOfWeek firstDay)
+		{
+			Calendar calendario = DateTimeFormatInfo.CurrentInfo.Calendar;
+			return calendario.GetWeekOfYear (date, CalendarWeekRule.FirstFullWeek, firstDay);
+		}
+
+		/// <summary>
+		/// Consider all the day .
+		/// </summary>
+		/// <returns>The day untill 23:59:59 hs.</returns>
+		/// <param name="date">Date.</param>
+		public static DateTime AllDay (this DateTime date)
+		{
+			return date.Date.AddDays(1).AddMilliseconds(-1);
+		}
+
+		/// <summary>
+		/// Firsts the day of month from date.
+		/// </summary>
+		/// <returns>The first day of month.</returns>
+		/// <param name="date">Date.</param>
+		public static DateTime FirstDayOfMonth(this DateTime date)
+		{
+			return new DateTime(date.Year, date.Month, 1);
+		}
+	}
+}
---
+++ Hamekoz/Extensions/StringExtensions.cs
@@ -0,0 +1,54 @@
+﻿//
+//  StringExtensions.cs
+//
+//  Author:
+//       Claudio Rodrigo Pereyra Diaz <rodrigo@hamekoz.com.ar>
+//
+//  Copyright (c) 2014 Hamekoz
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+namespace Hamekoz.Extensions
+{
+	public class StringExtensions
+	{
+		/// <summary>
+		/// String to the camel case convension.
+		/// </summary>
+		/// <returns>String camelized.</returns>
+		/// <param name="the_string">The string.</param>
+		public static string ToCamelCase(this string the_string)
+		{
+			// If there are 0 or 1 characters, just return the string.
+			if (the_string == null || the_string.Length < 2) return the_string;
+
+			// Split the string into words.
+			string[] words = the_string.Split(
+				new char[] { },
+				StringSplitOptions.RemoveEmptyEntries);
+
+			// Combine the words.
+			string result = words[0].ToLower();
+			for (int i = 1; i < words.Length; i++)
+			{
+				result +=
+					words[i].Substring(0, 1).ToUpper() +
+					words[i].Substring(1);
+			}
+
+			return result;
+		}
+	}
+}
