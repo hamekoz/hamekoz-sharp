c9c34df5bd45f0e76921fe1473ccf8f0e7bc19bc
Emiliano Canedo <emilianocanedo@gmail.com>
2014-11-17 10:43:03 -0300

    Change name of MasterWindow

--- Hamekoz.UI.Gtk/gtk-gui/Hamekoz.UI.Gtk.MasterWindow.cs
+++ Hamekoz.UI.Gtk/gtk-gui/Hamekoz.UI.Gtk.MasterWindow.cs
@@ -27,7 +27,7 @@ namespace Hamekoz.UI.Gtk
 			global::Stetic.Gui.Initialize (this);
 			// Widget Hamekoz.UI.Gtk.MasterWindow
 			this.Name = "Hamekoz.UI.Gtk.MasterWindow";
-			this.Title = global::Mono.Unix.Catalog.GetString ("MasterWindow");
+			this.Title = global::Mono.Unix.Catalog.GetString ("Postres Balcarce");
 			this.WindowPosition = ((global::Gtk.WindowPosition)(3));
 			// Container child Hamekoz.UI.Gtk.MasterWindow.Gtk.Container+ContainerChild
 			this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
