e73c71180915001d4a7f64899c441afad092df71
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2015-04-09 15:48:02 -0300

    Add more Icons

--- Hamekoz.UI/Icons/Icons.cs
+++ Hamekoz.UI/Icons/Icons.cs
@@ -24,13 +24,21 @@ namespace Hamekoz.UI
 {
 	public static class Icons
 	{
-		public static readonly Image Document = Image.FromResource ("Hamekoz.UI.Icons.x-office-document.png");
-		public static readonly Image Spreadsheet = Image.FromResource ("Hamekoz.UI.Icons.x-office-spreadsheet.png");
 		public static readonly Image New = Image.FromResource ("Hamekoz.UI.Icons.document-new.png");
+		public static readonly Image PrintPreview = Image.FromResource ("Hamekoz.UI.Icons.document-print-preview.png");
+		public static readonly Image Edit = Image.FromResource ("Hamekoz.UI.Icons.document-properties.png");
 		public static readonly Image Save = Image.FromResource ("Hamekoz.UI.Icons.document-save.png");
 		public static readonly Image SaveAs = Image.FromResource ("Hamekoz.UI.Icons.document-save-as.png");
-		public static readonly Image UserInfo = Image.FromResource ("Hamekoz.UI.Icons.user-info.png");
+		public static readonly Image Delete = Image.FromResource ("Hamekoz.UI.Icons.edit-delete.png");
 		public static readonly Image EditFind = Image.FromResource ("Hamekoz.UI.Icons.edit-find.png");
+		public static readonly Image EditFindReplace = Image.FromResource ("Hamekoz.UI.Icons.edit-find-replace.png");
+		public static readonly Image EditSelectAll = Image.FromResource ("Hamekoz.UI.Icons.edit-select-all.png");
+		public static readonly Image ProcessStop = Image.FromResource ("Hamekoz.UI.Icons.process-stop.png");
+		public static readonly Image Starred = Image.FromResource ("Hamekoz.UI.Icons.starred.png");
+		public static readonly Image SystemSearch = Image.FromResource ("Hamekoz.UI.Icons.system-search.png");
+		public static readonly Image UserInfo = Image.FromResource ("Hamekoz.UI.Icons.user-info.png");
+		public static readonly Image Document = Image.FromResource ("Hamekoz.UI.Icons.x-office-document.png");
+		public static readonly Image Spreadsheet = Image.FromResource ("Hamekoz.UI.Icons.x-office-spreadsheet.png");
 	}
 }
 
