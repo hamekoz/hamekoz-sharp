e30b9bff62009b4ebfeb0916acc5f0fc7d7ccfb6
Mariano Ripa <ripamariano@gmail.com>
2016-07-20 14:28:05 -0300

    Sifere

---
+++ Hamekoz.Argentina/Sifere/Enumerados.cs
@@ -0,0 +1,74 @@
+﻿//
+//  Enumerados.cs
+//
+//  Author:
+//       Mariano Ripa <ripamariano@gmail.com>
+//
+//  Copyright (c) 2016 Hamekoz
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+namespace Hamekoz.Argentina.Sifere
+{
+
+	public enum TipoDeOperacion
+	{
+		Retencion = 1,
+		Percepcion = 2,
+	}
+
+	public enum TipoDeComprobante
+	{
+		Factura = 1,
+		NotaDeDebito = 2,
+		OrdenDePago = 3,
+		BoletaDeDeposito = 4,
+		LiquidacionDePago = 5,
+		CertificadoDeObra = 6,
+		Recibo = 7,
+		ContDeLocacionDeServicio = 8,
+		OtroComprobante = 9,
+	}
+
+	public enum Letra
+	{
+		A = 'A',
+		B = 'B',
+		C = 'C',
+		M = 'M',
+	}
+
+	public enum TipoDeDocumento
+	{
+		CUIT = 3,
+		CUIL = 2,
+		CDI = 1,
+	}
+
+	public enum SituacionIngresosBrutos
+	{
+		Local = 1,
+		ConvenioMultilateral = 2,
+		NoInscripto = 4,
+		RegimenSimplificado = 5,
+	}
+
+	public enum SituacionFrenteAlIVA
+	{
+		ResponsableInscripto = 1,
+		Excento = 2,
+		Monotributo = 4,
+	}
+}
