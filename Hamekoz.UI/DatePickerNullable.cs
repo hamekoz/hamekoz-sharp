ccb277495726622f0e55da11cdfbde4a549172a7
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2016-11-08 16:01:17 -0300

    Agregado DatePickerNullable precario

---
+++ Hamekoz.UI/DatePickerNullable.cs
@@ -0,0 +1,62 @@
+﻿//
+//  DatePickerNullable.cs
+//
+//  Author:
+//       Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
+//
+//  Copyright (c) 2016 Hamekoz - www.hamekoz.com.ar
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using Xwt;
+
+namespace Hamekoz.UI
+{
+	//TODO implementar las propiedades normales de un DatePicker
+	public class DatePickerNullable : HBox
+	{
+		public DateTime? DateTime {
+			get {
+				return check.Active ? datepicker.DateTime : (DateTime?)null;
+			}
+			set {
+				check.Active = value.HasValue;
+				if (check.Active) {
+					datepicker.Sensitive = value.HasValue;
+					datepicker.DateTime = value.Value;
+				}
+			}
+		}
+
+		CheckBox check = new CheckBox ();
+		DatePicker datepicker = new DatePicker {
+			Sensitive = false,
+		};
+
+		
+		public DatePickerNullable ()
+		{
+			PackStart (check);
+			PackStart (datepicker);
+
+			check.Toggled += Check_Toggled;
+		}
+
+		void Check_Toggled (object sender, EventArgs e)
+		{
+			datepicker.Sensitive = ((CheckBox)sender).Active;
+		}
+	}
+}
+
