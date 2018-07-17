7eec7a2fa0c668d9c66ab462481813a410f7a478
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2018-07-17 11:37:39 -0300

    Marcada clase como obsoleta
    Signed-off-by: Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>

--- Hamekoz.Shared/Clases/LoteDeTarjeta.cs
+++ Hamekoz.Shared/Clases/LoteDeTarjeta.cs
@@ -18,12 +18,13 @@
 //
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-using Hamekoz.Core;
 using System;
+using Hamekoz.Core;
 
 namespace Hamekoz.Negocio
 {
 	//TODO ver si es posible unificar con PagoItem
+	[Obsolete ("Realmente no se utiliza para hacer sequimiento, y podria omitirse completamente")]
 	public partial class LoteDeTarjeta : IIdentifiable, IPersistible
 	{
 		#region IIdentifiable implementation
